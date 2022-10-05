6ede21e815de6a6d4fa1d287e5327846a08fe45d
ShadXo <gold_guver@hotmail.com>
2022-10-06 00:43:23 +0200

    Minor fixes in fork finder

--- find_fork.sh
+++ find_fork.sh
@@ -17,33 +17,38 @@ fi
 
 for (( ; ; ))
 do
+  echo "Running check on $WALLET"
+  echo "Starting from block $BLOCK"
 
 	for (( ; ; ))
 	do
-		echo "Starting from $BLOCK"
-
 		for FILE in $(ls ~/bin/${NAME}-cli_$WALLET.sh | sort -V); do
       echo "*******************************************"
-			echo "FILE: $FILE"
-			#BLOCKHASHEXPLORER=$(~/bin/${NAME}-cli_$WALLET.sh getblockhash $BLOCK)
-			BLOCKHASHEXPLORER=$(curl -s4 https://dogec.flitswallet.app/api/block/$BLOCK | jq -r ".hash")
-			#BLOCKHASHEXPLORER=$(curl -s4 https://api2.dogecash.org/height/$BLOCK | jq -r ".result.hash")
-			BLOCKHASHWALLET=$($FILE getblockhash $BLOCK)
+      echo "Checking block $BLOCK"
+			#echo "FILE: $FILE"
 
+			#BLOCKHASHEXPLORER=$(curl -s4 https://api2.dogecash.org/height/$BLOCK | jq -r ".result.hash")
+      BLOCKHASHEXPLORER=$(curl -s4 https://dogec.flitswallet.app/api/block/$BLOCK | jq -r ".hash")
 			if [ -z "$BLOCKHASHEXPLORER" ]; then
 			  break 2
 			fi
+      if [ "$BLOCKHASHEXPLORER" == null ]; then
+        echo "NO FORK FOUND"
+			  break 2
+			fi
 
-			if [ -z "$BLOCKHASHWALLET" ]; then
+      #BLOCKHASHWALLET=$(~/bin/${NAME}-cli_$WALLET.sh getblockhash $BLOCK)
+      BLOCKHASHWALLET=$($FILE getblockhash $BLOCK)
+      if [ -z "$BLOCKHASHWALLET" ]; then
 			  break 2
 			fi
 
 			echo "BLOCKHASHEXPLORER=$BLOCKHASHEXPLORER"
-			echo "BLOCKHASHWALLET=$BLOCKHASHWALLET"
+      echo "BLOCKHASHWALLET=$BLOCKHASHWALLET"
 		done
 
 		if [ "$BLOCKHASHWALLET" != "$BLOCKHASHEXPLORER" ]; then
-		  echo "FORK ON $BLOCK !!!!"
+		  echo "FORK FOUND ON BLOCK $BLOCK !!!!"
 		  break
 		fi
 
